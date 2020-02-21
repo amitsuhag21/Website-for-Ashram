a66c84bf469fa98f70a5c657f063f752050de5fc
Ashish Rawat <ashishrawat@D110194.local>
2020-02-21 07:18:37 +0530

    changes

--- cmsadmin/templates/footer.php
+++ cmsadmin/templates/footer.php
@@ -5,22 +5,22 @@
 
 <!--end-Footer-part-->
 
-<script src="http://lh-marathicms.htmedia.in/assets/js/jquery.min.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/jquery.ui.custom.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/bootstrap.min.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/bootstrap-colorpicker.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/bootstrap-datepicker.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/jquery.datetimepicker.full.min.js?v=1.6"></script> 
+<script src="./assets/js/jquery.min.js?v=1.6"></script> 
+<script src="./assets/js/jquery.ui.custom.js?v=1.6"></script> 
+<script src="./assets/js/bootstrap.min.js?v=1.6"></script> 
+<script src="./assets/js/bootstrap-colorpicker.js?v=1.6"></script> 
+<script src="./assets/js/bootstrap-datepicker.js?v=1.6"></script> 
+<script src="./assets/js/jquery.datetimepicker.full.min.js?v=1.6"></script> 
 
-<!-- <script src="http://lh-marathicms.htmedia.in/assets/js/jquery.toggle.buttons.js?v=1.6"></script>  -->
-<script src="http://lh-marathicms.htmedia.in/assets/js/masked.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/jquery.uniform.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/select2.min.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/matrix.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/matrix.form_common.js?v=1.6"></script> 
-<script src="http://lh-marathicms.htmedia.in/assets/js/jquery.peity.min.js?v=1.6"></script>
-<script type="text/javascript" src="http://lh-marathicms.htmedia.in/assets/ckeditor/ckeditor.js?v4"></script>
-<script type="text/javascript" src="http://lh-marathicms.htmedia.in/assets/ckfinder/ckfinder.js?v4"></script>
+<!-- <script src="./assets/js/jquery.toggle.buttons.js?v=1.6"></script>  -->
+<script src="./assets/js/masked.js?v=1.6"></script> 
+<script src="./assets/js/jquery.uniform.js?v=1.6"></script> 
+<script src="./assets/js/select2.min.js?v=1.6"></script> 
+<script src="./assets/js/matrix.js?v=1.6"></script> 
+<script src="./assets/js/matrix.form_common.js?v=1.6"></script> 
+<script src="./assets/js/jquery.peity.min.js?v=1.6"></script>
+<script type="text/javascript" src="./assets/ckeditor/ckeditor.js?v4"></script>
+<script type="text/javascript" src="./assets/ckfinder/ckfinder.js?v4"></script>
 
 <script type="text/javascript">
 function showhide(ids)
@@ -38,46 +38,7 @@ function showhide(ids)
   }
 }
 
-$( "#related" ).click(function() {  
-      if(document.getElementById("related").checked == true){  
-      var type='';
-      var gallerytype='';
-       if(window.location.href.indexOf("photostory") > -1) {
-       type='photo_story';
-       }
-       if(window.location.href.indexOf("news") > -1) {
-       type='news';
-       }
-        if(window.location.href.indexOf("gallery") > -1) {
-	  type='gallery';
-	  gallerytype='image';
-	   if(window.location.href.indexOf("video") > -1) {
-	   gallerytype='video';
-	   }
-       }
-      $("#filtericon").html('<i class="fa fa-spinner fa-spin" style="color:#FF0000;font-size:18px"></i>');
-       $.ajax({
-                type:'POST',
-                url:'http://lh-marathicms.htmedia.in/news/get_filtered_relatednews',
-                data:{'startdate':'','enddate':'','searchtitle':'','type':type,'main_category':'','story_id':'','gallery_type':gallerytype },
-                success:function(data){
-                    $('#foundrelated').html(data);
-                    $('#filtericon').html('<i class="fa fa-th"></i>');
-                }
-            });
-         }
-         else
-         {
-         if(confirm("Are you sure you want to remove related news?"))
-         {
-	    $('#foundrelated tr').remove();
-	    $('#sortable div').remove();
-	    $('#related_news').val('');
-         }
-         else
-         document.getElementById("related").checked=true;
-         }
-    });
+
     
     CKEDITOR.on('instanceReady', function(ev) {
         var editor = ev.editor;
@@ -90,7 +51,7 @@ $( "#related" ).click(function() {
                            //extract host names from URLs 
                             var hostname = (new URL(url)).hostname;
                            
-                            if (hostname !="www.livehindustan.com") {
+                            if (hostname !="") {
                                 element.attributes.rel = 'nofollow';
                             }
                         }
