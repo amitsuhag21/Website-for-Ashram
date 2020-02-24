2b13e283661bbb4d2642f7eb2138db5f74c1f839
Ashish Rawat <ashishrawat@D110194.local>
2020-02-24 11:10:45 +0530

    changes

--- cmsadmin/assets/js/category_widget.js
+++
@@ -1,8 +0,0 @@
-$("#field_type_category").on('click',function(){
-    $("#hashRow").hide();
-    $("#categoryRow").show();
-})
-$("#field_type_hash").on('click',function(){
-    $("#hashRow").show();
-    $("#categoryRow").hide();
-})
